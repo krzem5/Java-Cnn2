46f4a199a52f36b102e664f721585f1cc03afce9
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 15:41:43 +0100

    Push Update

---
+++ com/krzem/cnn2 - Copy/Random.java
@@ -0,0 +1,17 @@
+package com.krzem.cnn2 - Copy;
+
+
+
+
+
+
+public class Random{
+	public static final int SEED=12345;
+	private static java.util.Random RG=new java.util.Random(SEED);
+
+
+
+	public static double next(){
+		return Random.RG.nextGaussian();
+	}
+}
