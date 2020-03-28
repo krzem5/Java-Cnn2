d33b0f3740f48b13a5b3a18101a7b9c8675b2b16
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 15:40:45 +0100

    Push Update

---
+++ com/krzem/cnn2/Layer.java
@@ -0,0 +1,35 @@
+package com.krzem.cnn2;
+
+
+
+public interface Layer{
+	public int[] o_size(int[] ls);
+
+
+
+	public CNNVariable out(CNNVariable v);
+
+
+
+	public CNNVariable err(CNNVariable v,double lr);
+
+
+
+	public String _name();
+
+
+
+	public String _data();
+
+
+
+	public String _output(int[] ls);
+
+
+
+	public String _params(int[] ls);
+
+
+
+	public void _gen(int[] ls);
+}
